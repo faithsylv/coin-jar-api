cff0159d8143deeaf7efcace617b3544eaeab2f7
Maud Pie <maudpie@Maud.Pie>
2022-05-17 12:56:52 +1000

    Clean up configuration and rails built in comments

--- db/schema.rb
+++ db/schema.rb
@@ -1,19 +1,13 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 2022_03_28_063249) do
-
-  # These are extensions that must be enabled in order to support this database
+ActiveRecord::Schema.define(version: 2022_04_01_064213) do
   enable_extension "plpgsql"
+
+  create_table "jars", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "currency"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,5 +17,4 @@ ActiveRecord::Schema.define(version: 2022_03_28_063249) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "color"
   end
-
 end
